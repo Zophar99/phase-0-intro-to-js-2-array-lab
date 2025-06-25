c2cd5b434010a95bb5d7502795a03cdd259bb300
zophar <zophar.nyakundi@student.moringaschool.com>
2025-06-25 23:55:31 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats= [];{
+cats.length=0;
+}
+//console.log(cats.length)
+cats.push("Milo", "Otis", "Garfield");
+// cats.push()
+function destructivelyAppendCat (){
+cats.push ('Ralph');
+}
+//destructivelyAppendCat ()
+//console.log(cats)
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+//destructivelyPrependCat()
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+return [... cats,"Broom" ];
+
+}
+function prependCat(){
+    return ["Arnold",...cats ]
+}
+function removeLastCat(){
+    return cats.slice(0,- 1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
+console.log(cats.length)
+// let cats=['Milo', 'Otis', 'Garfield']
+
